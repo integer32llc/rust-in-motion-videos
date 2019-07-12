a11500147cc415230cc3bec3cf1a95ce5ca6aabb
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-07-12 15:50:57 -0400

    Add code from u4-m5

---
+++ unit4/module5-a-short-introduction-to-generics/05-38-invalid-usage/src/main.rs
@@ -0,0 +1,52 @@
+pub struct Approval<T> {
+    item: T,
+    approved: bool,
+}
+
+impl<T> Approval<T> {
+    pub fn new(item: T) -> Approval<T> {
+        Approval {
+            item,
+            approved: false,
+        }
+    }
+
+    pub fn approve(&mut self) {
+        self.approved = true;
+    }
+
+    pub fn replace(self, other_item: T) -> Approval<T> {
+        Approval {
+            item: other_item,
+            approved: self.approved,
+        }
+    }
+
+    pub fn approved_item(&self) -> Option<&T> {
+        if self.approved {
+            Some(&self.item)
+        } else {
+            None
+        }
+    }
+}
+
+use std::net::{IpAddr, Ipv4Addr};
+
+fn main() {
+    let amount = 1000;
+    let mut approval_amount = Approval::new(amount);
+    assert!(approval_amount.approved_item().is_none());
+    approval_amount.approve();
+    assert_eq!(approval_amount.approved_item(), Some(&1000));
+
+    let mut approval_ip = Approval::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)));
+    assert!(approval_ip.approved_item().is_none());
+    approval_ip.approve();
+    assert_eq!(
+        approval_ip.approved_item(),
+        Some(&IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)))
+    );
+
+    let replaced = approval_ip.replace(3);
+}
---
+++ unit4/module5-a-short-introduction-to-generics/README.md
@@ -0,0 +1,6 @@
+# Unit 4 - Module 5: A Short Introduction to Generics
+
+| Example | Description | Compiles? |
+|---------|-------------|-----------|
+| [01-36-approval](01-36-approval) | A type that enables approval of anything | ✅ |
+| [05-38-invalid-usage](05-38-invalid-usage) | Usage of the `Approval<T>` type that isn't valid according to the usage of generics in the definitions | ❌ |
