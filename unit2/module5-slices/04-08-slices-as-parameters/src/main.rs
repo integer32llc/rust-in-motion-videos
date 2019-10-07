7b51f2fb06e60ea627076961e98f7f419d73ae2b
Carol (Nichols || Goulding) <carol.nichols@gmail.com>
2019-10-07 12:57:13 -0400

    Update function names
    This will be updated in the videos soon.

--- unit2/module5-slices/04-08-slices-as-parameters/src/main.rs
+++ unit2/module5-slices/04-08-slices-as-parameters/src/main.rs
@@ -3,21 +3,21 @@ fn main() {
     let v = vec![4, 5, 6];
     let v_slice = &v[..];
 
-    only_array(&a);
-    only_vector(&v);
-    either_array_or_vector(&a[..]);
-    either_array_or_vector(&v[..]);
-    either_array_or_vector(&v_slice[0..1]);
+    only_reference_to_array(&a);
+    only_reference_to_vector(&v);
+    reference_to_either_array_or_vector(&a[..]);
+    reference_to_either_array_or_vector(&v[..]);
+    reference_to_either_array_or_vector(&v_slice[0..1]);
 }
 
-fn only_array(param: &[i32; 3]) {
+fn only_reference_to_array(param: &[i32; 3]) {
     println!("this is an array: {:?}", param);
 }
 
-fn only_vector(param: &Vec<i32>) {
+fn only_reference_to_vector(param: &Vec<i32>) {
     println!("this is a vector: {:?}", param);
 }
 
-fn either_array_or_vector(param: &[i32]) {
+fn reference_to_either_array_or_vector(param: &[i32]) {
     println!("this is a slice: {:?}", param);
 }
